9b134ded7c0adbf72177088a1ea81a3eebcf95cf
Kamil <alicesun65@gmail.com>
2024-10-09 10:02:20 +0300

    merge USEKANBANBACKEN-4 into dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypegooseModule } from "nestjs-typegoose";
+import { getMongoConfig } from "./db/mongo.config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
