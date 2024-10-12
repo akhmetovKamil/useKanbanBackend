92dbd549c496c671492cca78873160a2b92d9f38
Kamil <alicesun65@gmail.com>
2024-10-12 16:06:28 +0300

    Created at, rt guards and @Public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from "./app.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypegooseModule } from "nestjs-typegoose";
 import { getMongoConfig } from "./db/mongo.config";
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { AtGuard } from "./auth/guards/at.guard";
 
 @Module({
     imports: [
@@ -19,6 +21,12 @@ import { AuthModule } from './auth/auth.module';
         AuthModule,
     ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: AtGuard,
+        },
+    ],
 })
 export class AppModule {}
