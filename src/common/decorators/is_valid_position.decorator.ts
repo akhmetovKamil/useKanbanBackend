1c446f2804a3e7d35c9d16b361ee8eea41a1a2d2
Kamil <alicesun65@gmail.com>
2024-10-20 21:30:23 +0300

    implemented IsValidPosition decorator on class-validator

---
+++ src/common/decorators/is_valid_position.decorator.ts
@@ -0,0 +1,41 @@
+import {
+    registerDecorator,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+} from "class-validator";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "nestjs-typegoose";
+import { ReturnModelType } from "@typegoose/typegoose";
+import { ProjectsSchema } from "../../projects/projects.schema";
+
+@ValidatorConstraint({ async: true })
+@Injectable()
+export class IsValidPositionConstraint implements ValidatorConstraintInterface {
+    constructor(
+        @InjectModel(ProjectsSchema)
+        private readonly projectsModel: ReturnModelType<typeof ProjectsSchema>,
+    ) {}
+
+    async validate(position: string, args): Promise<boolean> {
+        const projectId = args.object.projectId;
+        const project = await this.projectsModel.findById(projectId);
+        return project ? project.positions.includes(position) : false;
+    }
+
+    defaultMessage(): string {
+        return "Position $value is not valid!"; // TODO Check error and fix
+    }
+}
+
+export function IsValidPosition(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: IsValidPositionConstraint,
+        });
+    };
+}
