f17bdd897e019af15189f571bc3e121129430849
Niyaz.Sadrutdinov <Niyaz.Sadrutdinov@tatar.ru>
2025-01-12 20:17:29 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
             transform: true,
         }),
     );
-    app.enableCors();
+    app.enableCors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    });
     app.setGlobalPrefix("api");
     await app.listen(3000);
 }
